d8346cae51af3d84bb7a3c488eb55bc339cfdd0d
victorfidelis <fideliscorrea.victor@gmail.com>
2024-02-17 18:34:02 -0300

    Documentação

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,16 @@ import 'package:orca_e_organiza/screens/event_screen.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 
+/*
+App organizador e orçador de eventos, reformas e afins.
+
+Através deste aplicativo o usuário cria eventos onde pode cadastrar qualquer tipo de tópico orçamentário 
+(chamados no app de modalidades). Dentro de cada modalidade pode-se cadastrar diversos orçamentos e 
+compará-los. Além disso, o usuário pode selecionar o melhor orçamento de cada modalidade, e, ao fazê-lo, o 
+aplicativo calcula o orçamento total do seu evento, fazendo com que o usuário tenha um controle total de 
+gastos.
+*/
+
 void main() {
   runApp(
     ChangeNotifierProvider(
